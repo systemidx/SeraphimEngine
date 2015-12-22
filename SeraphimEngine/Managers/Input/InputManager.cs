e391975a0adf8e46a2a1b5fdd26ff07d1af65db6
systemidx <bryon.weber@gmail.com>
2015-12-22 10:30:00 -0500

    Adding missed file.

--- SeraphimEngine/Managers/Input/InputManager.cs
+++ SeraphimEngine/Managers/Input/InputManager.cs
@@ -5,7 +5,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using MonoGame.Extended.InputListeners;
 using SeraphimEngine.Input;
 
 namespace SeraphimEngine.Managers.Input
