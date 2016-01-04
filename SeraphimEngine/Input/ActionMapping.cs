9b99aefbc26281bf3d7b58d453738399691d91ee
systemidx <bryon.weber@gmail.com>
2016-01-03 19:33:46 -0500

    Adding action mapping.

---
+++ SeraphimEngine/Input/ActionMapping.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace SeraphimEngine.Input
+{
+    /// <summary>
+    /// Class ActionMapping.
+    /// </summary>
+    public class ActionMapping : IActionMapping
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActionMapping" /> class.
+        /// </summary>
+        /// <param name="actionKey">The actionMapping key.</param>
+        /// <param name="actionButton">The actionMapping button.</param>
+        /// <param name="event">The event.</param>
+        public ActionMapping(Keys actionKey, Buttons actionButton, InputAction @event)
+        {
+            ActionKey = actionKey;
+            ActionButton = actionButton;
+            Event = @event;
+        }
+
+        public InputAction Event { get; private set; }
+
+        /// <summary>
+        /// Gets the actionMapping key.
+        /// </summary>
+        /// <value>The actionMapping key.</value>
+        public Keys ActionKey { get; private set; }
+
+        /// <summary>
+        /// Gets the actionMapping button.
+        /// </summary>
+        /// <value>The actionMapping button.</value>
+        public Buttons ActionButton { get; private set; } 
+    }
+}
